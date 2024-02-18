140f1486c84dd85fc0295458f60f01dfc7caf385
veyis <vakgul@gmail.com>
2024-02-18 13:31:30 -0800

    v24

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -2,9 +2,9 @@
 
 import { revalidatePath } from "next/cache";
 
-import User from "@/lib/database/models/user.model";
-import { connectToDatabase } from "@/lib/database/mongose";
-import { handleError } from "@/lib/utils";
+import User from "../database/models/user.model";
+import { connectToDatabase } from "../database/mongoose";
+import { handleError } from "../utils";
 
 // CREATE
 export async function createUser(user: CreateUserParams) {
@@ -44,7 +44,7 @@ export async function updateUser(clerkId: string, user: UpdateUserParams) {
     });
 
     if (!updatedUser) throw new Error("User update failed");
-
+    
     return JSON.parse(JSON.stringify(updatedUser));
   } catch (error) {
     handleError(error);
@@ -80,15 +80,14 @@ export async function updateCredits(userId: string, creditFee: number) {
 
     const updatedUserCredits = await User.findOneAndUpdate(
       { _id: userId },
-      { $inc: { creditBalance: creditFee } },
+      { $inc: { creditBalance: creditFee }},
       { new: true }
-    );
+    )
 
-    if (!updatedUserCredits) throw new Error("User credits update failed");
+    if(!updatedUserCredits) throw new Error("User credits update failed");
 
     return JSON.parse(JSON.stringify(updatedUserCredits));
   } catch (error) {
     handleError(error);
   }
 }
-
