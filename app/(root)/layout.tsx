cf5c342f9584c6a1cbb299f6c3588f288607e480
veyis <vakgul@gmail.com>
2024-02-18 12:20:56 -0800

    init

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,20 @@
+import Sidebar from "@/components/shared/Sidebar";
+import MobileNav from "@/components/shared/MobileNav";
+import React from "react";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="root">
+      <Sidebar />
+      <MobileNav />
+
+      <div className="root-container">
+        <div className="wrapper">{children}</div>
+      </div>
+      {/*       
+      <Toaster /> */}
+    </main>
+  );
+};
+
+export default Layout;
---
+++ app/(root)/transformations/[id]/update/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UpdateTransormationPage = () => {
+  return <div>Update TransormationPage</div>;
+};
+
+export default UpdateTransormationPage;
