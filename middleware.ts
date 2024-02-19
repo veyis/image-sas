21a1152e7e37de4e84d412c68fe31ba8496a19dd
veyis <vakgul@gmail.com>
2024-02-19 12:54:26 -0800

    v26

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,9 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/api/webhooks/clerk"],
-
-  
-
+  publicRoutes: ["/api/webhooks(.*)"],
 });
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-
 };
